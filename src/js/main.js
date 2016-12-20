3ed3baf4ba601834cbf341d880b1cbc553a89b3e
VsevolodBobrovsky <loyzov@yandex.ru>
2016-12-20 16:09:01 +0200

    'comm'

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 	//= partials/lang.js
+
 	//= partials/discount.js
 	//= partials/history.js
 	//= partials/clients.js
 	//= partials/team.js
 	//= partials/news.js
 	//= partials/blog.js
+
+	//= partials/question.js
+	//= partials/hr.js
+	//= partials/steps.js
+	//= partials/calc.js
+	//= partials/portfolio.js
+	//= partials/keys.js
+	//= partials/saloons.js
+
 	//= partials/contacts.js
 });
