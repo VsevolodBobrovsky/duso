0a140bfd7c2a4b6ef8e75db5b6deb534af6ecabb
VsevolodBobrovsky <loyzov@yandex.ru>
2016-12-29 17:14:58 +0200

    'com'

--- src/js/partials/keys.js
+++ src/js/partials/keys.js
@@ -1,19 +1,11 @@
-/*$('.keys-step').click(function(){
-	var ind = $(this).index();
-	$('.keys-step').removeClass('keys-step-active');
-	for(i=0; i < ind; i++){
-		$('.keys-step').eq(i).addClass('keys-step-active');
-	}
-	$('.keys-line').animate({ height: $(this).position().top + 5 }, 500);
-})*/
-
 $(window).scroll(function(){
 	var win = $(window).scrollTop();
 	var off = $('.keys').offset().top - 400;
-	if (win >= off){
-		$('.keys-line').animate({ height: $('.keys-step:last-child').position().top + 5 }, 5000);
-		for (i=0; i<5; i++){
-			$('.keys-step').eq(i).addClass('keys-step-active');
-		}
+	if (win > off){
+		$('.keys-line').animate({ height: $('.keys-step:last-child').position().top + 5 }, 10000);
+		setTimeout(function(){ $('.keys-step').eq(1).addClass('keys-step-active') }, 3500);
+		setTimeout(function(){ $('.keys-step').eq(2).addClass('keys-step-active') }, 5000);
+		setTimeout(function(){ $('.keys-step').eq(3).addClass('keys-step-active') }, 7000);
+		setTimeout(function(){ $('.keys-step').eq(4).addClass('keys-step-active') }, 10000);
 	}
 });
--- src/js/partials/portfolio.js
+++ src/js/partials/portfolio.js
@@ -1,2 +1,27 @@
 //= ../product/jquery.mCustomScrollbar.js
+//= ../product/slick.min.js
 $('.portfolio-half-left').mCustomScrollbar();
+$('.portfolio-inner-slider').slick({
+	prevArrow: $('.portfolio-inner-slider-arrow-left'),
+	nextArrow: $('.portfolio-inner-slider-arrow-right')
+});
+
+$('.portfolio-arrows .slider-arrow').click(function(){
+	var dir = $(this).attr('data-dir');
+	var ind = $('.portfolio-slider').children('.portfolio-slide-active').index();
+	var len = $('.portfolio-slide').length;
+	$('.portfolio-slide').removeClass('portfolio-slide-active');
+	$('.portfolio-dots .slider-dot').removeClass('slider-dot-active');
+	if (dir == 'next'){
+		var indNext = ind + 1;
+		if (indNext == len) indNext = 0;
+		$('.portfolio-dots .slider-dot[data-index="' + indNext + '"]').addClass('slider-dot-active');
+		$('.portfolio-slider').children('.portfolio-slide').eq(indNext).addClass('portfolio-slide-active');
+	}
+	else{
+		var indPrev = ind -1;
+		if (indPrev < 0) indPrev = len - 1;
+		$('.portfolio-dots .slider-dot[data-index="' + indPrev + '"]').addClass('slider-dot-active');
+		$('.portfolio-slider').children('.portfolio-slide').eq(indPrev).addClass('portfolio-slide-active');
+	}
+});
