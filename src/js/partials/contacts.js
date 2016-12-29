b2d4939387a2d57f467011ad17175929456d88e8
VsevolodBobrovsky <loyzov@yandex.ru>
2016-12-29 12:21:47 +0200

    'com'

--- src/js/partials/contacts.js
+++ src/js/partials/contacts.js
@@ -1,13 +1,14 @@
 $('.contacts-toggle-link').click(function(){
 	if($(this).hasClass('contacts-toggle-link-active') ) return false;
-	var ind = $(this).attr('data-city');
 	$('.contacts-toggle-link').removeClass('contacts-toggle-link-active');
 	$(this).addClass('contacts-toggle-link-active');
 	$('.contacts-map').animate({'opacity':'0'}, 200, function(){
-		$('.contacts-map').removeClass('contacts-map-active');
-		$('.contacts-map:nth-child(' + ind + ')').addClass('contacts-map-active');
-		});
+		
+	});
 	$('.contacts-map').animate({'opacity':'1'}, 200);
 	$('.contacts-addr').removeClass('contacts-addr-active');
-	$('.contacts-addr:nth-child(' + ind + ')').addClass('contacts-addr-active');
+	$('.contacts-addr:nth-child(' + $(this).index() + 1 + ')').addClass('contacts-addr-active');
 });
+
+var coordinateY = $('.contacts-toggle-link').eq(0).attr('data-coordinateY'),
+    coordinateX = $('.contacts-toggle-link').eq(0).attr('data-coordinateX');
--- src/js/partials/question.js
+++ src/js/partials/question.js
@@ -16,6 +16,11 @@ $('.question-dots .slider-dot').click(function(){
 	$('.question-slider').children('.question-slide').eq(ind).addClass('question-slide-active');
 	$('.question-dots .slider-dot').removeClass('slider-dot-active');
 	$(this).addClass('slider-dot-active');
+	$('.question-bg-img').animate({'opacity':'0'}, 0, function(){
+		$('.question-bg-img').removeClass('question-bg-img-active');
+		$('.question-bg-img').eq(ind).addClass('question-bg-img-active');
+	});
+	$('.question-bg-img').animate({'opacity':'1'}, 150);
 });
 
 var mousewheelevent = (/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel";
@@ -30,7 +35,7 @@ $('.question').bind(mousewheelevent, function(e){
 		if(e.originalEvent.detail < 0) $('.question-slider').children('.question-slide').eq(indPrev).addClass('question-slide-active')
 		else $('.question-slider').children('.question-slide').eq(indNext).addClass('question-slide-active');
 	}
-	if(mousewheelevent == 'mousewheel'){
+	else if(mousewheelevent == 'mousewheel'){
 		if(e.originalEvent.wheelDelta / 120 > 0) $('.question-slider').children('.question-slide').eq(indPrev).addClass('question-slide-active')
 		else $('.question-slider').children('.question-slide').eq(indNext).addClass('question-slide-active');
 	}
@@ -40,6 +45,11 @@ $('.question').bind(mousewheelevent, function(e){
 		$('.question-slider').children('.question-slide').eq(ind).addClass('question-slide-active');
 	}
 	$('.question-dots .slider-dot[data-index="' + ind + '"]').addClass('slider-dot-active');
+	$('.question-bg-img').animate({'opacity':'0'}, 0, function(){
+		$('.question-bg-img').removeClass('question-bg-img-active');
+		$('.question-bg-img').eq(ind).addClass('question-bg-img-active');
+	});
+	$('.question-bg-img').animate({'opacity':'1'}, 150);
 	if(indNext == dotsQuan){
 		$('html, body').animate({
 			scrollTop: $( $('.question').next() ).offset().top
